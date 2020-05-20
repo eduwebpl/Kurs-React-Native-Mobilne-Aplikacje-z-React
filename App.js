cb3a23439ff22abc5a75ccac3d5373bc1f9f2445
Ufos <>
2020-05-20 14:58:28 +0200

    static

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useRef} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -15,6 +15,7 @@ import {
   Text,
   StatusBar,
   Image,
+  Button,
 } from 'react-native';
 
 import {
@@ -33,12 +34,22 @@ import WebView from 'react-native-webview';
 const App = () => {
   console.log('Start log');
 
+  const webView = useRef(null);
+
+  const injectJs = `document.querySelectorAll('p').forEach(p => p.style.fontSize = '50px');`;
+
+  function onMakeitBigger() {
+    webView.current.injectJavaScript(injectJs);
+  }
+
   return (
     <SafeAreaView>
       <Container>
-        <DynamicWebView
-          source={{uri: 'https://wp.pl/'}}
+        <StaticWebView
+          ref={webView}
+          source={{html: '<h1>Title</h1><p>description</p>'}}
           startInLoadingState={true}
+          // injectedJavaScript={injectJs}
           renderLoading={() => <Text>{'Loading...'}</Text>}
           renderError={() => <Text>{'Error!'}</Text>}
           onShouldStartLoadWithRequest={request => {
@@ -54,6 +65,7 @@ const App = () => {
             console.log('WEBVIEW msg = ', msg);
           }}
         />
+        <Button title={'MAKE IT BIGGER!'} onPress={onMakeitBigger} />
       </Container>
     </SafeAreaView>
   );
@@ -64,9 +76,9 @@ const Container = styled(View)`
   height: 100%;
 `;
 
-const DynamicWebView = styled(WebView)`
+const StaticWebView = styled(WebView)`
   width: 100%;
-  height: 100%;
+  height: 300px;
 `;
 
 export default App;
